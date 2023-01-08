12e04fc1d83e9e0764c5d2bcc15ece990ad3e8fb
t-h-rk-turner <71654487+t-h-rk-turner@users.noreply.github.com>
2023-01-08 03:31:59 -0500

    remove underscore from proc parameters

--- src/nimcorpora.nim
+++ src/nimcorpora.nim
@@ -11,6 +11,10 @@ import nimcorpora/private/common
 type
   Corpora* = ref object
 
+
+using
+  corpora: Corpora
+
 func newCorpora*(): Corpora =
   ## Get a new Corpora object. Needed to access Corpora data.
   ## Instead of this proc, you could also use `new Corpora` instead.
@@ -20,7 +24,7 @@ func newCorpora*(): Corpora =
 
 # --- categories ---
 
-proc getCategories*(_: Corpora): seq[string] =
+proc getCategories*(corpora): seq[string] =
   setDataVar()
 
   ## Get all categories (directories) in the Corpora data.
@@ -29,75 +33,75 @@ proc getCategories*(_: Corpora): seq[string] =
     if collect(for file in walkFiles(fmt"{data / dir}/*.json"): file).len != 0:
       result.add dir
 
-proc categories*(_: Corpora): seq[string] =
+proc categories*(corpora): seq[string] =
   ## Alias for `getCategories <#getCategories,Corpora>`_
 
-  getCategories(_)
+  getCategories(corpora)
 
 # --- subcategories ---
 
-proc getSubcategories*(_: Corpora): seq[string] =
+proc getSubcategories*(corpora): seq[string] =
   ## Get all subcategories (a.k.a the json files in the data) under all categories
   setDataVar()
 
   for file in walkDirRec(data): 
     result.add file.split(DirSep)[^2..^1].join($DirSep).changeFileExt("")
 
-proc getSubcategories*(_: Corpora; category: string): seq[string] =
+proc getSubcategories*(corpora; category: string): seq[string] =
   ## Get all subcategories under `category`
   setDataVar()
 
   for file in walkDirRec(data / category): 
     result.add file.split(DirSep)[^2..^1].join($DirSep).changeFileExt("")
 
-proc getSubcategories*(_: Corpora; categories: openArray[string]): seq[seq[string]] =
+proc getSubcategories*(corpora; categories: openArray[string]): seq[seq[string]] =
   ## Get all subcategories under `categories`
 
-  for category in categories: result.add _.getSubcategories(category)
+  for category in categories: result.add corpora.getSubcategories(category)
 
-proc subcategories*(_: Corpora): seq[string] =
+proc subcategories*(corpora): seq[string] =
   ## Alias for `getSubcategories <#getSubcategories,Corpora>`_
 
-  getSubcategories(_)
+  getSubcategories(corpora)
 
 # --- retreive data ---
 
-proc getFile*(_: Corpora; path: string): JsonNode =
+proc getFile*(corpora; path: string): JsonNode =
   ## Get a file by its path. Input `path` as subcategory (e.x "animals/ant_anatomy")
   setDataVar()
 
   parseFile(data / path.changeFileExt(".json"))
 
-proc getFile*(_: Corpora; category, subcategory: string): JsonNode =
+proc getFile*(corpora; category, subcategory: string): JsonNode =
   ## Get a file by its category and subcategory
 
-  _.getFile(category / subcategory)
+  corpora.getFile(category / subcategory)
 
-proc getFiles*(_: Corpora; paths: openArray[string]): seq[JsonNode] =
+proc getFiles*(corpora; paths: openArray[string]): seq[JsonNode] =
   ## Get multiple files by their paths.
 
-  for path in paths: result.add _.getFile(path)
+  for path in paths: result.add corpora.getFile(path)
 
-proc getFiles*(_: Corpora; category: string): seq[JsonNode] =
+proc getFiles*(corpora; category: string): seq[JsonNode] =
   ## Get all files under `category`
 
-  for subcategory in _.getSubcategories(category): 
-    result.add _.getFile(subcategory)
+  for subcategory in corpora.getSubcategories(category): 
+    result.add corpora.getFile(subcategory)
 
-proc getFilesByCategories*(_: Corpora; categories: openArray[string]): seq[seq[JsonNode]] =
+proc getFilesByCategories*(corpora; categories: openArray[string]): seq[seq[JsonNode]] =
   ## Get all files under `categories`.
 
-  for category in categories: result.add _.getFiles(category)
+  for category in categories: result.add corpora.getFiles(category)
 
-proc getFiles*(_: Corpora; category: string; subcategories: openArray[
+proc getFiles*(corpora; category: string; subcategories: openArray[
     string]): seq[JsonNode] =
   ## Get multiple files from `subcategories` under `category`
 
-  for subcategory in subcategories: result.add _.getFile(category / subcategory)
+  for subcategory in subcategories: result.add corpora.getFile(category / subcategory)
 
 # --- update/reinstall data ---
 
-proc update*(_: Corpora; output: bool = off) =
+proc update*(corpora; output: bool = off) =
   ## Update Corpora data
 
   when defined(release):
